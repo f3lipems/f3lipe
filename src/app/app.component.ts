c6a5f1e3304f1dd76df3f3bae1766cdbeee1f180
Felipe <felipe@visionar.io>
2025-04-29 18:25:35 -0700

    add main components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Components
+import { NavbarComponent } from '../app/components/navbar/navbar.component';
+import { HeaderComponent } from '../app/components/header/header.component';
+import { FooterComponent } from '../app/components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
