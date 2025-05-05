a11ed5e7b1c4743db9cb15970b84194ddcebbe0a
Felipe <felipe@visionar.io>
2025-05-05 11:35:54 -0700

    refactor header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { CarroselIconsComponent } from '../carrosel-icons/carrosel-icons.component';  
+import { CarroselIconsComponent } from '../carrosel-icons/carrosel-icons.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [CarroselIconsComponent],
+  imports: [CarroselIconsComponent, MatButtonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
