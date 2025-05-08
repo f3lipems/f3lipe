48f94bcf97b97bbdf3dcc56f527a48e8f21e0174
Felipe <felipe@visionar.io>
2025-05-08 19:59:33 -0300

    refactor animation time

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -17,10 +17,10 @@ import {
     trigger('textSwitch', [
       transition(':enter', [
         style({ transform: 'translateY(100%)', opacity: 0 }),
-        animate('500ms ease-out', style({ transform: 'translateY(0)', opacity: 1 }))
+        animate('700ms ease-out', style({ transform: 'translateY(0)', opacity: 1 }))
       ]),
       transition(':leave', [
-        animate('500ms ease-in', style({ transform: 'translateY(-100%)', opacity: 0 }))
+        animate('700ms ease-in', style({ transform: 'translateY(-100%)', opacity: 0 }))
       ])
     ])
   ]
